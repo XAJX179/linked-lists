3864f58b1214a1bb5219f8307b494cf4fab26db7
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2024-12-10 23:06:23 +0530

    Feat : create LinkedList class

---
+++ lib/linked_list.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# class LinkedList for creating linked list with help of Node class's objects
+class LinkedList
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+end
